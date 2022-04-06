759ffbb2dd61289241e47e3f5055d9a692b1e821
stevan <stevangrubac123@gmail.com>
2022-04-06 19:06:06 -0400

    added url to database

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from auth import API_KEY
 import os
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cocktail'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///cocktail')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secretttt')
